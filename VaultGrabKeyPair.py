6494dd6df2d3d0cccbf7cc1bf796a8fd12275e65
Cloud42 <hello@cloud42.io>
2021-01-23 15:38:50 +0000

    Use std mount path

--- VaultGrabKeyPair.py
+++ VaultGrabKeyPair.py
@@ -36,13 +36,13 @@ def _vaultcreds():
   try:
     client = hvac.Client(url=vault_addr)
     client.auth_userpass(USER.strip(), PASS)
-    list_roles_response = client.secrets.aws.list_roles(mount_point='customaws')
+    list_roles_response = client.secrets.aws.list_roles(mount_point='aws')
     list_user_roles = list_roles_response['data']['keys']
     length = len(list_user_roles)
     for i in range(length):
       print("ROLE: " + list_user_roles[i] + " - available ")
     role = input("Type the role to use: ")
-    gen_creds_response = client.secrets.aws.generate_credentials(name=role.strip(), mount_point='customaws')
+    gen_creds_response = client.secrets.aws.generate_credentials(name=role.strip(), mount_point='aws')
     access=gen_creds_response['data']['access_key']
     secret=gen_creds_response['data']['secret_key']
     print("Generated access / secret keys:\n" +
